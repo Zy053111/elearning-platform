a08e4951ff982de67c5db862f1ba692ded54bd5e
Zy053111 <113881878+Zy053111@users.noreply.github.com>
2026-03-06 22:12:07 +0800

    Fix asgi.py import order

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,23 +1,17 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-from chat.middleware import JWTAuthMiddleware # Import your new middleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from chat.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": JWTAuthMiddleware(
         AuthMiddlewareStack(
             URLRouter(
